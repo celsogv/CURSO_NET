7e08a0680667526cb18bf714c9f526406f4adf60
celso <celso149@gmail.com>
2021-02-16 09:45:20 +0100

    soluciones

--- solucion3/ejercicio_metodos/Program.cs
+++ solucion3/ejercicio_metodos/Program.cs
@@ -4,9 +4,17 @@ namespace ejercicio_metodos
 {
     class Program
     {
+
+        private string nombre;
+        private int edad;
+        private Decimal nota;
+
+    
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Introduce el nombre: ");
+            nombre = Console.ReadLine();
+
         }
     }
 }
