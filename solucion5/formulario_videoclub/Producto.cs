db1166e8558a70cf0a30ed5b9a3c38c6c3a86cf9
celso <celso149@gmail.com>
2021-02-23 10:26:11 +0100

    wpf

---
+++ solucion5/formulario_videoclub/Producto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formulario_videoclub
+{
+    class Producto
+    {
+    }
+}
