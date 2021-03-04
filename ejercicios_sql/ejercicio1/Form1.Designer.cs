92f14952ac490a80912859f9a216b2876daaec72
celso <celso149@gmail.com>
2021-03-04 08:17:33 +0100

    linq

---
+++ ejercicios_sql/ejercicio1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtApellido1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtApellido2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtTelefono = new System.Windows.Forms.TextBox();
+            this.BtnNuevo = new System.Windows.Forms.Button();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.Actualizar = new System.Windows.Forms.Button();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtBuscar = new System.Windows.Forms.TextBox();
+            this.GridDatos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.GridDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(136, 15);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(100, 22);
+            this.TxtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(407, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Apellido Paterno";
+            // 
+            // TxtApellido1
+            // 
+            this.TxtApellido1.Location = new System.Drawing.Point(548, 21);
+            this.TxtApellido1.Name = "TxtApellido1";
+            this.TxtApellido1.Size = new System.Drawing.Size(100, 22);
+            this.TxtApellido1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Apellido Materno";
+            // 
+            // TxtApellido2
+            // 
+            this.TxtApellido2.Location = new System.Drawing.Point(235, 89);
+            this.TxtApellido2.Name = "TxtApellido2";
+            this.TxtApellido2.Size = new System.Drawing.Size(100, 22);
+            this.TxtApellido2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(455, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Telefono";
+            // 
+            // TxtTelefono
+            // 
+            this.TxtTelefono.Location = new System.Drawing.Point(586, 92);
+            this.TxtTelefono.Name = "TxtTelefono";
+            this.TxtTelefono.Size = new System.Drawing.Size(100, 22);
+            this.TxtTelefono.TabIndex = 7;
+            // 
+            // BtnNuevo
+            // 
+            this.BtnNuevo.Location = new System.Drawing.Point(101, 209);
+            this.BtnNuevo.Name = "BtnNuevo";
+            this.BtnNuevo.Size = new System.Drawing.Size(86, 23);
+            this.BtnNuevo.TabIndex = 8;
+            this.BtnNuevo.Text = "Nuevo";
+            this.BtnNuevo.UseVisualStyleBackColor = true;
+            this.BtnNuevo.Click += new System.EventHandler(this.BtnNuevo_Click);
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Location = new System.Drawing.Point(277, 209);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(83, 23);
+            this.BtnGuardar.TabIndex = 9;
+            this.BtnGuardar.Text = "Guardar";
+            this.BtnGuardar.UseVisualStyleBackColor = true;
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // Actualizar
+            // 
+            this.Actualizar.Location = new System.Drawing.Point(444, 209);
+            this.Actualizar.Name = "Actualizar";
+            this.Actualizar.Size = new System.Drawing.Size(88, 23);
+            this.Actualizar.TabIndex = 10;
+            this.Actualizar.Text = "Actualizar";
+            this.Actualizar.UseVisualStyleBackColor = true;
+            this.Actualizar.Click += new System.EventHandler(this.Actualizar_Click);
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(611, 209);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(82, 23);
+            this.BtnEliminar.TabIndex = 11;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(112, 335);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Buscar";
+            // 
+            // TxtBuscar
+            // 
+            this.TxtBuscar.Location = new System.Drawing.Point(214, 332);
+            this.TxtBuscar.Name = "TxtBuscar";
+            this.TxtBuscar.Size = new System.Drawing.Size(253, 22);
+            this.TxtBuscar.TabIndex = 13;
+            // 
+            // GridDatos
+            // 
+            this.GridDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridDatos.Location = new System.Drawing.Point(151, 375);
+            this.GridDatos.Name = "GridDatos";
+            this.GridDatos.RowHeadersWidth = 51;
+            this.GridDatos.RowTemplate.Height = 24;
+            this.GridDatos.Size = new System.Drawing.Size(683, 207);
+            this.GridDatos.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 594);
+            this.Controls.Add(this.GridDatos);
+            this.Controls.Add(this.TxtBuscar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.Actualizar);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.BtnNuevo);
+            this.Controls.Add(this.TxtTelefono);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtApellido2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtApellido1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+
+            ((System.ComponentModel.ISupportInitialize)(this.GridDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtApellido1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtApellido2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtTelefono;
+        private System.Windows.Forms.Button BtnNuevo;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.Button Actualizar;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtBuscar;
+        private System.Windows.Forms.DataGridView GridDatos;
+    }
+}
+
---
+++ ejercicios_sql/ejercicio1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio1
+{
+    public partial class Form1 : Form
+    {
+
+        DataClasses1DataContext ClienteLinq = new DataClasses1DataContext();
+        public Form1()
+        {
+            InitializeComponent();
+            this.listarclientes();
+
+
+
+        }
+
+
+
+
+        void listarclientes() {
+            ClienteLinq.listartodos();
+            GridDatos.DataSource = ClienteLinq.listartodos();
+
+        
+        }
+
+
+
+
+        private void BtnNuevo_Click(object sender, EventArgs e)
+        {
+            TxtNombre.Clear();
+            TxtApellido1.Clear();
+            TxtApellido2.Clear();
+            TxtTelefono.Clear();
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            ClienteLinq.insertarCliente(TxtNombre.Text, TxtApellido1.Text, TxtApellido2.Text, TxtTelefono.Text);
+            this.listarclientes();
+        }
+
+        private void Actualizar_Click(object sender, EventArgs e)
+        {
+            ClienteLinq.Modificarcliente(TxtNombre.Text, TxtApellido1.Text, TxtApellido2.Text, TxtTelefono.Text);
+            this.listarclientes();
+
+
+
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            ClienteLinq.EliminarCliente(TxtNombre.Text);
+            this.listarclientes();
+
+        }
+    }
+}
